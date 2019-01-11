e92ad2d763b9ab630d99bdd22a63d7ea9efed795
bieshan <hpv7xwnpuvm64@yahoo.co.jp>
2019-01-11 23:17:23 +0900

    add: add entrance ui

---
+++ src/components/ui.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+
+export class MenuBar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="nav-wrapper container" />
+      </div>
+    );
+  }
+}
---
+++ src/pages/entrance.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { MenuBar } from "../components/ui";
+
+export class Entrance extends Component {
+  render() {
+    return (
+      <div>
+        <MenuBar />
+      </div>
+    );
+  }
+}
